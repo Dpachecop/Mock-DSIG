4ccab8d9c370019750cbd2e4813d3fb9ded7cea3
Samuel <sagole2006gmail.com>
2026-03-30 21:26:27 -0500

    fix: Se reasigno la pantalla inicial de ejecucion(habia sido cambiada para probar las demas pantallas)

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Mock_DSIG
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MenuInicialAdministrador());
+            Application.Run(new principal_screen());
         }
     }
 }
